1efd5b97a09d3185f486e0d1354d81238521b1d2
jdpra <jdpra@bobby>
2022-12-01 14:23:47 +0530

    updated

--- src/test/java/project/Exp11/AppTest.java
+++ src/test/java/project/Exp11/AppTest.java
@@ -1,20 +1,23 @@
 package project.Exp11;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test() {
+
+        App p = new App();
+        assertEquals("Hello DURGA", p.getMessage("DURGA"));
+
+    }
+    @Test
+    public void testNameEmpty() {
+
+        App c = new App();
+        assertEquals("Please provide a name!", c.getMessage(" "));
+
     }
 }
