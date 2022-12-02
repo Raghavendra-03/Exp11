8557ae11661e0537c5a24c61181354f247a11bbb
jdpra <jdpra@bobby>
2022-12-02 20:48:40 +0530

    exp11

--- src/main/java/project/Exp11/App.java
+++ src/main/java/project/Exp11/App.java
@@ -1,20 +1,18 @@
 package project.Exp11;
 
+/**
+ * Hello world!
+ *
+ */
 public class App 
 {
-    public String getMessage(String name) {
-
-        StringBuilder result = new StringBuilder();
-
-        if (name == null || name.trim().length() == 0) {
-
-            result.append("Please provide a name!");
-
-        } else {
-
-            result.append("Hello " + name);
-
-        }
-        return result.toString();
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to devops lab" );
+        
+    }
+    public int add(int a,int b)
+    {
+         return(a+b);    	
     }
 }
--- src/test/java/project/Exp11/AppTest.java
+++ src/test/java/project/Exp11/AppTest.java
@@ -4,20 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ **/
 public class AppTest 
 {
+    /**
+     * Rigorous Test 🙂
+     */
     @Test
-    public void test() {
-
-        App p = new App();
-        assertEquals("Hello DURGA", p.getMessage("DURGA"));
-
-    }
-    @Test
-    public void testNameEmpty() {
-
-        App c = new App();
-        assertEquals("Please provide a name!", c.getMessage(" "));
-
+    public void test()
+    {
+    	App a=new App();
+    	int actual=a.add(15, 15);
+    	int expected=30;
+    	assertEquals(expected,actual);
+        
     }
 }
